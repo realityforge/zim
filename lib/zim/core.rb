956249cc305ae3f8949e79908aa0215bfc3a72d8
Peter Donald <peter@realityforge.org>
2015-05-26 12:20:40 +1000

    Add core element

--- lib/zim/core.rb
+++ lib/zim/core.rb
@@ -1,4 +1,29 @@
 module Zim
+
+  # Base class used for elements configurable via options
+  class ConfigElement
+
+    def initialize(options, &block)
+      self.options = options
+      yield self if block_given?
+    end
+
+    def options=(options)
+      options.each_pair do |k, v|
+        keys = k.to_s.split('.')
+        target = self
+        keys[0, keys.length - 1].each do |target_accessor_key|
+          target = target.send target_accessor_key.to_sym
+        end
+        begin
+          target.send "#{keys.last}=", v
+        rescue NoMethodError
+          raise "Attempted to configure property \"#{keys.last}\" on #{self.class} but property does not exist."
+        end
+      end
+    end
+  end
+
   class << self
     def context(&block)
       self.instance_eval &block
